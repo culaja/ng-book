dbdd05a4890712f793ed0e460ba83782d6be4b5c
Stanko Culaja <culaja@gmail.com>
2017-04-18 14:11:57 +0200

    Added skeleton forms application. Next steps are to make some forms.

---
+++ form-app/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { SkuFormComponent } from './sku-form/sku-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SkuFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ form-app/src/app/sku-form/sku-form.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sku-form',
+  templateUrl: './sku-form.component.html',
+})
+export class SkuFormComponent {
+}
